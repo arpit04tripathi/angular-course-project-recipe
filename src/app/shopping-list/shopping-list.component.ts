a94aa85bb4825d4a8265f3423be0905194635f1b
Arpit Tripathi <arpit04tripathi@gmail.com>
2020-07-04 20:55:40 +0530

    shared model

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from "../shared/ingredient.model"
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.sass']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient("Poha", 1.5),
+    new Ingredient("Peanuts", .5)
+  ];
 
   constructor() { }
 
